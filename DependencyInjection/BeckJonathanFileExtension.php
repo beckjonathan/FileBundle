18f7c108d9598e7feada9f10dad8aedfcb9a6868
beckjonathan <bckjonathan@gmail.com>
2013-08-16 18:48:42 +0200

    Update BeckJonathanFileExtension.php

--- DependencyInjection/BeckJonathanFileExtension.php
+++ DependencyInjection/BeckJonathanFileExtension.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace BeckJonathan\FileBundle\DependencyInjection;
+namespace BeckJonathan\Bundle\FileBundle\DependencyInjection;
 
 use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\Config\FileLocator;
